d9b567d782cb2581420c29162ca5d34c7bac12cf
1269047508@qq.com <1269047508@qq.com>
2026-01-19 00:30:52 +0800

    fix: use legacy CSP for certificate and absolute paths

--- .github/workflows/create_cert.ps1
+++ .github/workflows/create_cert.ps1
@@ -1,3 +1,4 @@
-$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=SlideSCITemp" -KeySpec Signature
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=SlideSCITemp" -KeySpec Signature -KeyAlgorithm RSA -KeyLength 2048 -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider"
 $password = ConvertTo-SecureString -String "password" -Force -AsPlainText
-Export-PfxCertificate -Cert $cert -FilePath "SlideSCI_Temp.pfx" -Password $password
+$pfxPath = Join-Path $env:GITHUB_WORKSPACE "SlideSCI_Temp.pfx"
+Export-PfxCertificate -Cert $cert -FilePath $pfxPath -Password $password
