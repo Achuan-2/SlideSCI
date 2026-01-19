0ffc4eb1ffb0ed1cf681de6da2e775f3da36b7b7
1269047508@qq.com <1269047508@qq.com>
2026-01-19 12:04:08 +0800

    fix: place certificate in project dir to satisfy MSBuild relative path requirements

--- .github/workflows/create_cert.ps1
+++ .github/workflows/create_cert.ps1
@@ -1,4 +1,5 @@
 $cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=SlideSCITemp" -KeySpec Signature -KeyAlgorithm RSA -KeyLength 2048 -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider"
 $password = ConvertTo-SecureString -String "password" -Force -AsPlainText
-$pfxPath = Join-Path $env:GITHUB_WORKSPACE "SlideSCI_Temp.pfx"
+# Move PFX to the project directory specifically
+$pfxPath = Join-Path $env:GITHUB_WORKSPACE "SlideSCI\SlideSCI_Temp.pfx"
 Export-PfxCertificate -Cert $cert -FilePath $pfxPath -Password $password
