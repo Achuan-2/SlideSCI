81da76cfa7549c5c62ca63dd815d7c657936bcd1
1269047508@qq.com <1269047508@qq.com>
2026-01-19 12:11:20 +0800

    fix: switch to certificate thumbprint signing to bypass file import issues

--- .github/workflows/create_cert.ps1
+++ .github/workflows/create_cert.ps1
@@ -1,5 +1,8 @@
-$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=SlideSCITemp" -KeySpec Signature -KeyAlgorithm RSA -KeyLength 2048 -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider"
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=SlideSCITemp" -KeySpec Signature -KeyAlgorithm RSA -KeyLength 2048 -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" -HashAlgorithm SHA256
 $password = ConvertTo-SecureString -String "password" -Force -AsPlainText
 # Move PFX to the project directory specifically
 $pfxPath = Join-Path $env:GITHUB_WORKSPACE "SlideSCI\SlideSCI_Temp.pfx"
 Export-PfxCertificate -Cert $cert -FilePath $pfxPath -Password $password
+
+# Output Thumbprint to GITHUB_ENV
+echo "PFX_THUMBPRINT=$($cert.Thumbprint)" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8
